6a727e9117ccb5c92409658dadd0f40db357970a
Slavag76 <slavag76@yandex.ru>
2020-10-30 13:11:49 +0300

    Задание сделано полностью, включая *

--- src/stream1/lesson6/Main.java
+++ src/stream1/lesson6/Main.java
@@ -7,8 +7,9 @@ public class Main {
 
         Animal cat = new Cat(10, 20, 1, "Кот", 200, 0, 2);
         Animal dog = new Dog(300, 5, 20, "Собака", 500, 10, 0.5f);
+        Animal dog1 = new Dog(20,20,20,"Другая собака", 10, 10, 30 );
 
-        Animal[] animals = {cat, dog};
+        Animal[] animals = {cat, dog, dog1};
 
         for (Animal pets : animals) {
             System.out.println(pets.typeAnimal + " " + pets.runrun(pets.run_distance) + "\n");
